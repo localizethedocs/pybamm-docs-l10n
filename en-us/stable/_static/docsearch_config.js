b91bafc5e051c82d6fd2ea1e30a33abc056b35e6
ltdrobot <ltdrobot@localizethedocs.org>
2026-09-21 16:25:51 +0000

    Deploying to pages from @ localizethedocs/pybamm-docs-l10n@a94f3acecd7fbd2c3ee7542f99a90c4f2cecaf84 🚀

---
+++ en-us/stable/_static/docsearch_config.js
@@ -0,0 +1,7 @@
+docsearch({
+  container: "#algolia-docsearch",
+  appId: "BXYTEF2JI8",
+  apiKey: "b7e7f1fc1a7c40a1587e52e8f4ff3b45",
+  indexName: "pybamm",
+  placeholder: "Search the PyBaMM documentation",
+});
